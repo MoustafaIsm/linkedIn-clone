8ec71cd88ef52916c1d0c18aee608fd0f9155125
Moustafa <moustafaism013@hotmail.com>
2022-10-14 17:30:08 +0300

    Frontend => Added more styling for the navigation buttons

--- frontend/src/components/home/NavButton.jsx
+++ frontend/src/components/home/NavButton.jsx
@@ -1,8 +1,11 @@
 
 function NavButton({ title }) {
     return (
-        <li className="w-3/4 flex m-auto items-center p-4 hover:cursor-pointer hover:bg-white">
-            <img src={`/${title}.png`} alt={title} className="w-8" />
+        <li className="w-3/4 flex m-auto items-center p-4 hover:cursor-pointer hover:bg-white transition-all duration-500 rounded-2xl">
+            <img
+                src={`/${title}.png`}
+                alt={title}
+                className="w-8" />
             <p className="text-xl capitalize"> {title} </p>
         </li>
     )
