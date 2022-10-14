e5a6a79051a3ec9b76dea798f094f2dfddab1e03
Moustafa <moustafaism013@hotmail.com>
2022-10-14 13:25:26 +0300

    Frontend => Added a login route and login page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return (
-    <div className="text-green-400">
-      App
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Navigate to={'/login'} />} />
+                <Route path='/login' element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
