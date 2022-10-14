b2dd26433b4b95daef3be982ba6d43196522e3b3
Moustafa <moustafaism013@hotmail.com>
2022-10-14 20:26:18 +0300

    Frontend => Added a logout button

--- frontend/src/components/home/TopNavigation.jsx
+++ frontend/src/components/home/TopNavigation.jsx
@@ -5,13 +5,16 @@ function TopNavigation() {
     const [search, setSearch] = useState('');
 
     return (
-        <div className="ml-72 p-3">
+        <div className="ml-72 p-3 flex justify-between">
             <input
                 type='text'
                 placeholder='Search'
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
                 className='p-1 px-2 border-2 rounded border-none bg-blue-100 focus:outline-gray-300' />
+            <button className="uppercase py-2 px-5 border-primary-blue border-2 text-black rounded-lg hover:bg-primary-blue hover:text-white">
+                Logout
+            </button>
         </div>
     )
 }
