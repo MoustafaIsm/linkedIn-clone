171a7e200e1041202496a14a57a6fb08e190cded
Moustafa <moustafaism013@hotmail.com>
2022-10-14 21:56:12 +0300

    Frontend => Added a message input and send button

--- frontend/src/components/home/Message.jsx
+++ frontend/src/components/home/Message.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 
 function Message() {
+
+    const [message, setMessage] = useState('');
+
     return (
-        <div>Message</div>
+        <div className="relative h-full">
+            {/* Message input */}
+            <div className="flex gap-2 absolute bottom-0 w-full z-10">
+                <input
+                    type='text'
+                    placeholder='Message'
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    className='p-1 px-2 border-2 rounded border-primary-blue focus:outline-gray-300 flex-grow' />
+                <button className="bg-primary-blue text-white uppercase font-bold p-2 px-4 rounded hover:cursor-pointer hover:bg-secondary-blue transition-all duration-300">Send</button>
+            </div>
+            {/* Messages */}
+            <div className="flex flex-col overflow-y-scroll">
+
+            </div>
+        </div>
     )
 }
 
