f439a72fb3c37436ca99e799c0ba843d4f2a86e8
Moustafa <moustafaism013@hotmail.com>
2022-10-14 19:25:55 +0300

    Frontend => Added the functionality of the side nav button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import Header from './components/login/Header';
-import Footer from './components/login/Footer';
-import LoginForm from './components/login/LoginForm';
-import SignupForm from './components/login/SignupForm';
+import Header from './components/access/Header';
+import Footer from './components/access/Footer';
+import LoginForm from './components/access/LoginForm';
+import SignupForm from './components/access/SignupForm';
 import SideNavigation from "./components/home/SideNavigation";
 import Feed from "./pages/Feed";
 import Messages from "./pages/Messages";
@@ -26,22 +26,21 @@ function App() {
                     <>
                         <SideNavigation />
                         <Routes>
-                            <Route path="/" element={<Navigate to={'/feed'} />} />
-                            <Route path="/feed" element={<Feed />} />
+                            <Route path="/" element={<Feed />} />
+                            <Route path="/home" element={<Feed />} />
                             <Route path="/messages" element={<Messages />} />
                             <Route path="/notifications" element={<Notifications />} />
-                            <Route path="/user_profile" element={<UserProfile />} />
+                            <Route path="/profile" element={<UserProfile />} />
                             <Route path="/other_profile" element={<OtherProfile />} />
                         </Routes>
                     </>
-
                     :
                     <div className="bg-beige flex flex-col h-screen">
                         <Header />
                         <div className="flex-grow px-10 flex flex-col items-center">
                             <p className="text-4xl my-6">Make the most of your professional life</p>
                             <Routes>
-                                <Route path="/" element={<Navigate to={'/login'} />} />
+                                <Route path="/" element={<LoginForm />} />
                                 <Route path='/login' element={<LoginForm />} />
                                 <Route path="/signup" element={<SignupForm />} />
                             </Routes>
--- frontend/src/components/home/NavButton.jsx
+++ frontend/src/components/home/NavButton.jsx
@@ -1,7 +1,16 @@
+import { useNavigate } from "react-router-dom"
 
-function NavButton({ title }) {
+function NavButton({ title, active, changeActive }) {
+    const navigate = useNavigate();
+
+    const changePage = () => {
+        changeActive(title);
+        navigate(`/${title}`);
+    }
+
     return (
-        <li className="w-3/4 flex m-auto items-center p-4 hover:cursor-pointer hover:bg-white transition-all duration-500 rounded-2xl">
+        <li className={`w-3/4 flex m-auto items-center p-4 hover:cursor-pointer hover:bg-white transition-all duration-300 rounded-2xl ${active === title ? 'bg-white font-bold' : ''}`}
+            onClick={changePage}>
             <img
                 src={`/${title}.png`}
                 alt={title}
