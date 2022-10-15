df19d5f414daa81d95ea8b12b3832158cda5dc71
Moustafa <moustafaism013@hotmail.com>
2022-10-15 07:42:47 +0300

    Frontend => Added the notification card in the notification tab

--- frontend/src/pages/Notifications.jsx
+++ frontend/src/pages/Notifications.jsx
@@ -1,9 +1,20 @@
+import NotificationCard from "../components/home/NotificationCard"
 
 function Notifications() {
     return (
         <div className="ml-72 bg-beige p-4 ">
             <p className="font-bold text-3xl ">Notifications</p>
-
+            <div className="grid grid-cols-2 gap-4 py-4">
+                <NotificationCard />
+                <NotificationCard />
+                <NotificationCard />
+                <NotificationCard />
+                <NotificationCard />
+                <NotificationCard />
+                <NotificationCard />
+                <NotificationCard />
+                <NotificationCard />
+            </div>
         </div>
     )
 }
