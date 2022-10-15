31d769c82e44a3e88440e6e8f3939b0f3ee7e596
Moustafa <moustafaism013@hotmail.com>
2022-10-15 19:22:35 +0300

    Backend => Added the users model with its schema

---
+++ linkedin-server/models/users.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true, select: false },
+    location: { type: String, default: 'NA' },
+    date_of_birth: { type: Date, default: getDate() },
+    education: { type: String, default: 'NA' },
+    degree: { type: String, default: 'NA' },
+    cv_url: { type: String, default: 'NA' },
+    profile_picture_url: { type: String, default: 'NA' },
+    works_at: { type: String, default: 'NA' }
+});
+
+const getDate = () => {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear();
+    return dd + '-' + mm + '-' + yyyy;
+}
