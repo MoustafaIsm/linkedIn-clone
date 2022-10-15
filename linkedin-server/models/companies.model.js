7f93d600c3a9214718046fd523ea8e23c172411e
Moustafa <moustafaism013@hotmail.com>
2022-10-15 19:31:33 +0300

    Backend => Added the company schema

---
+++ linkedin-server/models/companies.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const companySchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true },
+    location: { type: String, default: 'NA' },
+    date_of_creation: { type: Date, default: '00-00-0000' },
+    feild: { type: String, default: 'NA' },
+    nbr_of_employees: { type: Number, default: 0 },
+    is_recruiting: { type: Boolean, default: false },
+    profile_picture_url: { type: String, default: 'NA' },
+});
