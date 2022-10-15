c752597aee22038108710dfd616d47e72c1fa8d6
Moustafa <moustafaism013@hotmail.com>
2022-10-15 09:43:55 +0300

    Frontend => Added different user info if the user is a company or not

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -5,8 +5,43 @@ function UserProfile() {
 
     const [isCompany, setIsCompany] = useState(true);
 
+    let userInfo = '';
+    if (isCompany) {
+        userInfo =
+            <div className="grid grid-cols-2 flex-grow text-xl px-4">
+                <div className="flex flex-col gap-4">
+                    <p className="font-bold">Name</p>
+                    <p>Email</p>
+                    <p>Location</p>
+                    <p>Date of Creation</p>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <p>Feild</p>
+                    <p>Number of employees</p>
+                    <p>Recruiting</p>
+                </div>
+            </div>
+            ;
+    } else {
+        userInfo =
+            <div className="grid grid-cols-2 flex-grow text-xl px-4">
+                <div className="flex flex-col gap-4">
+                    <p className="font-bold">Name</p>
+                    <p>Email</p>
+                    <p>Location</p>
+                    <p>Date of birth</p>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <p>Education</p>
+                    <p>Degree</p>
+                    <p>CV</p>
+                    <p>Work</p>
+                </div>
+            </div>;
+    }
+
     useEffect(() => {
-        // setIsCompany(localStorage.getItem('isCompany'));
+        setIsCompany(localStorage.getItem('isCompany'));
     }, []);
 
     return (
@@ -22,20 +57,7 @@ function UserProfile() {
 
                 </div>
                 {/* User info */}
-                <div className="grid grid-cols-2 flex-grow text-xl px-4">
-                    <div className="flex flex-col gap-4">
-                        <p className="font-bold">Name</p>
-                        <p>Email</p>
-                        <p>Location</p>
-                        <p>Date of birth</p>
-                    </div>
-                    <div className="flex flex-col gap-4">
-                        <p>Education</p>
-                        <p>Degree</p>
-                        <p>CV</p>
-                        <p>Work</p>
-                    </div>
-                </div>
+                {userInfo}
             </div>
             {
                 isCompany ?
