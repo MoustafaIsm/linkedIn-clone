82a66e9ac72966201aba77a9bf9a3db15fb28d13
Moustafa <moustafaism013@hotmail.com>
2022-10-16 14:52:24 +0300

    Backend => Added the remove follow api

--- linkedin-server/controllers/follow.controller.js
+++ linkedin-server/controllers/follow.controller.js
@@ -2,7 +2,7 @@ const User = require('../models/users.model');
 const Company = require('../models/companies.model');
 const Follow = require('../models/follows.model');
 
-const addFollow = async (req, res) => {
+const addFollow = (req, res) => {
     const { company_id } = req.params;
     Follow.create({
         user: req.body.user.id,
@@ -17,7 +17,27 @@ const addFollow = async (req, res) => {
         .catch((error) => {
             res.status(400).json({
                 status: 'Failed',
-                message: 'Couldn\'t update user',
+                message: 'Couldn\'t add follow',
+            })
+        });
+}
+
+const removeFollow = (req, res) => {
+    const { company_id } = req.params;
+    Follow.findOneAndDelete({
+        user: req.body.user.id,
+        following: company_id
+    })
+        .then(() => {
+            res.json({
+                status: 'Success',
+                message: 'Follow removed successfully'
+            });
+        })
+        .catch((error) => {
+            res.status(400).json({
+                status: 'Failed',
+                message: 'Couldn\'t remove follow',
             })
         });
 }
