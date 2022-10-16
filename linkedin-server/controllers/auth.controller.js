ebcfd3270412434bd0903490e78f3e2afa2ec254
Moustafa <moustafaism013@hotmail.com>
2022-10-16 08:38:43 +0300

    Backend => Added the register api in tche auth controller

--- linkedin-server/controllers/auth.controller.js
+++ linkedin-server/controllers/auth.controller.js
@@ -2,15 +2,16 @@ const User = require('../models/users.model');
 const Company = require('../models/companies.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const Company = require('../models/companies.model');
 
 const login = async (req, res) => {
     const { email, password, isCompany } = req.body;
     let user;
 
     if (isCompany)
-        user = Company.findOne({ email }).select('+password');
+        user = await Company.findOne({ email }).select('+password');
     else
-        user = User.findOne({ email }).select('+password');
+        user = await User.findOne({ email }).select('+password');
 
     if (!user)
         return res.status(404).json({
@@ -35,4 +36,40 @@ const login = async (req, res) => {
             token: token
         }
     });
+}
+
+const register = async (req, res) => {
+    const { isCompany } = req.body;
+    let user;
+    try {
+        if (isCompany) {
+            const { name, email, password } = req.body;
+            const user = new Company();
+            user.name = name;
+            user.email = email;
+            user.password = await bcrypt.hash(password, 10);
+            await user.save();
+
+        } else {
+            const { first_name, last_name, email, password } = req.body;
+            const user = new Company();
+            user.first_name = first_name;
+            user.last_name = last_name;
+            user.email = email;
+            user.password = await bcrypt.hash(password, 10);
+            await user.save();
+        }
+        res.json({
+            status: 'Success',
+            message: 'Register succeded',
+            data: {
+                user: user
+            }
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'Failed',
+            message: err.message
+        });
+    }
 }
