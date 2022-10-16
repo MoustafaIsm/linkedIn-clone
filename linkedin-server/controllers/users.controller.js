551641d9cd06cb04c539bc1a3867fa1b083b081d
Moustafa <moustafaism013@hotmail.com>
2022-10-16 11:55:48 +0300

    Backend => Added the update user api

--- linkedin-server/controllers/users.controller.js
+++ linkedin-server/controllers/users.controller.js
@@ -12,6 +12,37 @@ const getUserByToken = async (req, res) => {
     });
 }
 
+const updateUser = async (req, res) => {
+    const { id, ...data } = req.body.user;
+    const newValues = req.body.update;
+    if (req.body.is_company) {
+        try {
+            const company = await Company.findOneAndUpdate(id, {
+                name: newValues.name,
+                location: newValues.location,
+                date_of_creation: newValues.date_of_creation,
+                feild: newValues.feild,
+                nbr_of_employees: newValues.nbr_of_employees,
+                is_recruiting: newValues.is_recruiting,
+                profile_picture_url: newValues.profile_picture_url
+            });
+            return res.json({
+                status: 'Success',
+                message: 'Updated user succesfully',
+                data: {
+                    user: company
+                }
+            });
+        } catch (error) {
+            return res.status(400).json({
+                status: 'Failed',
+                message: 'Couldn\'t update user succesfully',
+                error: error
+            });
+        }
+    }
+}
+
 module.exports = {
     getUserByToken
 }
